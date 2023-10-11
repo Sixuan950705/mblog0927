796e6a6a91452f7b544885068344e6ad5a08a1f9
Sixuan950705 <145543268+Sixuan950705@users.noreply.github.com>
2023-10-11 16:32:56 +0800

    Add files via upload

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 200)
+    slug = models.CharField(max_length = 200)
+    body = models.TextField()
+    pub_data = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-pub_data', )
+
+    def __str__(self) -> str:
+        return self.title
